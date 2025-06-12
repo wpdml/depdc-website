c9228420ee9c30bbabba3aac07aaf01cc1f63565
Jeui <jeui4433@gmail.com>
2025-06-13 02:47:03 +0900

    donate

--- src/Pages/DonatePage.js
+++ src/Pages/DonatePage.js
@@ -1,13 +1,41 @@
-import "./Donatepage.css"
+import { useEffect } from "react";
+import "./Donatepage.css";
 
 const DonatePage = () => {
+
+    const createParticle = () => {
+    const particle = document.createElement("div");
+    particle.className = "particle";
+    const size = Math.random() * 1.5 + 1.3;
+    particle.style.width = `${size}px`;
+    particle.style.height = `${size}px`;
+    particle.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+    particle.style.left = `${Math.random() * 100}vw`;
+    particle.style.top = `${Math.random() * 100}vh`;
+    particle.style.animation = `glitter ${
+      Math.random() * 6 + 2
+    }s ease-in-out forwards`;
+    particle.style.zIndex = 1000;
+    document.body.appendChild(particle);
+    const glowIntensity = Math.random() * 15 + 20;
+    particle.style.boxShadow = `0 0 ${glowIntensity}px rgba(255, 255, 255)`;
+    particle.addEventListener("animationend", () => {
+      particle.remove();
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(createParticle, 80);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="donate">
       <div className="donate-box">
-        COMING SOON!
+        <div className="donate-text"> COMING SOON!</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DonatePage
+export default DonatePage;
