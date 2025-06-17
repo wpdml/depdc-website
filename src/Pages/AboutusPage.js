cbed8944c346b0ca9289586d6272acf14e821636
Jeui <jeui4433@gmail.com>
2025-06-17 21:52:57 +0900

    j

--- src/Pages/AboutusPage.js
+++ src/Pages/AboutusPage.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./AboutusPage.css";
 
 const AboutusPage = () => {
   return (
-    <div>
-      aboutus
+    <div className="about-box">
+      <div className="intro-box">
+        <div className="intro-img">img</div>
+        <div className="intro-texts">
+          <p>The Ban Mai Roy Project</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+      <div className="contact-box">
+        <div className="contact-left">
+          <p>Contact us!</p>
+          <p>Please fill out the form below to send us an email.</p>
+          <p>E-mail:</p>
+          <p>jaitothanida@gmail.com</p>
+          <p>Instagram:</p>
+          <p>@banmairoy</p>
+        </div>
+        <div className="contact-right">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Subject" />
+          <textarea placeholder="Message" />
+          <button>Submit</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutusPage
+export default AboutusPage;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./Homepage.css";
 
 const Homepage = () => {
   return (
-    <div>
-      hello
+    <div class="home-box">
+      <div class="top-box">
+        <p>Empowerment.</p>
+        <p>Education.</p>
+        <p>Embrace.</p>
+      </div>
+      <div class="bottom-box">
+        <div class="item-box">
+          <div>
+            Empowering stateless youth at risk to human trafficking in Northern
+            Thailand through education, mentorship, and community connection. We
+            aim to create safe spaces where young people can grow, express
+            themselves, and build a sense of belonging
+          </div>
+          <button>Learn more</button>
+        </div>
+        <div class="item-box">image</div>
+        <div class="item-box">image</div>
+        <div class="item-box">image</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
