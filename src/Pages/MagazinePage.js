a86b4049b3f438d07b7054b3ceda391ed701ec3c
Jeui <jeui4433@gmail.com>
2025-09-04 14:07:19 +0900

    issuee

--- src/Pages/MagazinePage.js
+++ src/Pages/MagazinePage.js
@@ -12,11 +12,28 @@ const MagazinePage = () => {
             <div className="mag-sub">A space for blooming voices</div>
           </div>
           <div className="page right-page">
-            <img src={x} alt="stamp" className="magazine-sticker" loading="eager"/>
-            <div className="coming-text">COMING SOON</div>
+            <img
+              src={x}
+              alt="stamp"
+              className="magazine-sticker"
+              loading="eager"
+            />
+            <div className="coming-text">First issue out now!</div>
           </div>
         </div>
       </div>
+      <div className="magazine-flip">
+        <div className="first-issue">THE AMARANTH MAGAZINE · Summer 2025 Vol. 1, Issue 1</div>
+        <iframe
+          src="https://online.fliphtml5.com/qmvzu/drgb/#p=1"
+          width="100%"
+          height="100%"
+          style={{ border: "none" }}
+          allowFullScreen
+          title="Flipbook Magazine"
+        ></iframe>
+      </div>
+      <div className="issue-soon">NEXT ISSUE COMING SOON!</div>
     </div>
   );
 };
